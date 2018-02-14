8d354e8811f09f9970e8d858c980afed057a32c5
Igor Prus <amitator@gmail.com>
2018-02-14 16:20:12 +0300

    added music

--- core/src/ru/geekbrains/spacegame/core/screen/GameScreen.java
+++ core/src/ru/geekbrains/spacegame/core/screen/GameScreen.java
@@ -2,6 +2,7 @@ package ru.geekbrains.spacegame.core.screen;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -36,6 +37,7 @@ public class GameScreen extends Base2DScreen{
     private ExplosionPool explosionPool;
 
     private Sound soundExplosion;
+    private Music music;
 
     public GameScreen(Game game) {
         super(game);
@@ -45,6 +47,9 @@ public class GameScreen extends Base2DScreen{
     public void show() {
         super.show();
         soundExplosion = Gdx.audio.newSound(Gdx.files.internal("sounds/explosion.wav"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        music.setLooping(true);
+        music.play();
         backgroundTexture = new Texture("stars.jpg");
         background = new Background(new TextureRegion(backgroundTexture));
         atlas = new TextureAtlas("mainAtlas.tpack");
